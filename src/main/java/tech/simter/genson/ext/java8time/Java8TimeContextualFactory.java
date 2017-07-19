13e0f54c565b97edf089a19395086dd362a5b903
RJ.HWang <rongjihuang@gmail.com>
2017-07-19 18:31:50 +0800

    Refactor Java8TimeContextualFactory to cache converter

--- src/main/java/tech/simter/genson/ext/java8time/Java8TimeContextualFactory.java
+++ src/main/java/tech/simter/genson/ext/java8time/Java8TimeContextualFactory.java
@@ -13,7 +13,10 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.Temporal;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 /**
  * Add {@link java.time} support for Genson.
@@ -24,40 +27,56 @@ import java.util.Locale;
 @Named
 @Singleton
 public class Java8TimeContextualFactory implements ContextualFactory {
+  private static Map<String, Converter<?>> converters = new HashMap<>();
+
   @Override
   public Converter create(BeanProperty property, Genson genson) {
+    if (!Temporal.class.isAssignableFrom(property.getRawClass())) return null; // only deal with java.time
+
     String format = null, lang = null;
-    Format cfg1 = property.getAnnotation(Format.class); // support simter Format annotation
+    Format cfg1 = property.getAnnotation(Format.class);                   // support simter Format annotation
     if (null != cfg1) {
       format = cfg1.value();
       lang = cfg1.lang();
     } else {
-      // support genson JsonDateFormat annotation
-      JsonDateFormat cfg2 = property.getAnnotation(JsonDateFormat.class);
+      JsonDateFormat cfg2 = property.getAnnotation(JsonDateFormat.class); // support genson JsonDateFormat annotation
       if (null != cfg2) {
         format = cfg2.value();
         lang = cfg2.lang();
       }
     }
-    if (LocalDate.class.isAssignableFrom(property.getRawClass())) {
-      return new LocalDateConverter(createFormatter(format, lang));
-    } else if (LocalDateTime.class.isAssignableFrom(property.getRawClass())) {
-      return new LocalDateTimeConverter(createFormatter(format, lang));
-    } else if (LocalTime.class.isAssignableFrom(property.getRawClass())) {
-      return new LocalTimeConverter(createFormatter(format, lang));
-    } else if (java.time.ZonedDateTime.class.isAssignableFrom(property.getRawClass())) {
-      return new ZonedDateTimeConverter(createFormatter(format, lang));
-    } else if (java.time.OffsetDateTime.class.isAssignableFrom(property.getRawClass())) {
-      return new OffsetDateTimeConverter(createFormatter(format, lang));
-    } else if (java.time.OffsetTime.class.isAssignableFrom(property.getRawClass())) {
-      return new OffsetTimeConverter(createFormatter(format, lang));
-    } else if (java.time.YearMonth.class.isAssignableFrom(property.getRawClass())) {
-      return new YearMonthConverter(createFormatter(format, lang));
-    }
-    return null;
+
+    return getConverter(property.getRawClass(), format, lang);
   }
 
-  private DateTimeFormatter createFormatter(String format, String lang) {
+  private static Converter<?> getConverter(Class<?> type, String format, String lang) {
+    String id = type.getName() + "_" + format + "_" + lang;
+    // If exists just return it to avoid repeat instance
+    if (converters.containsKey(id)) return converters.get(id);
+
+    // instance and cache
+    Converter<?> c;
+    if (LocalDate.class.isAssignableFrom(type)) {
+      converters.put(id, c = new LocalDateConverter(createFormatter(format, lang)));
+    } else if (LocalDateTime.class.isAssignableFrom(type)) {
+      converters.put(id, c = new LocalDateTimeConverter(createFormatter(format, lang)));
+    } else if (LocalTime.class.isAssignableFrom(type)) {
+      converters.put(id, c = new LocalTimeConverter(createFormatter(format, lang)));
+    } else if (java.time.ZonedDateTime.class.isAssignableFrom(type)) {
+      converters.put(id, c = new ZonedDateTimeConverter(createFormatter(format, lang)));
+    } else if (java.time.OffsetDateTime.class.isAssignableFrom(type)) {
+      converters.put(id, c = new OffsetDateTimeConverter(createFormatter(format, lang)));
+    } else if (java.time.OffsetTime.class.isAssignableFrom(type)) {
+      converters.put(id, c = new OffsetTimeConverter(createFormatter(format, lang)));
+    } else if (java.time.YearMonth.class.isAssignableFrom(type)) {
+      converters.put(id, c = new YearMonthConverter(createFormatter(format, lang)));
+    } else {
+      c = null;
+    }
+    return c;
+  }
+
+  private static DateTimeFormatter createFormatter(String format, String lang) {
     if (format == null || format.isEmpty()) return null;
     else {
       Locale locale = lang.isEmpty() ? null : new Locale(lang);
